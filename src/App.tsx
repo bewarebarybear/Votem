a4d75576c599838270d2e5c32e0d13d00b10f363
bewarebarybear <bewarebarybear@gmail.com>
2026-08-01 14:29:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,8 @@ export default function App() {
     {
       date: '2026-08-01',
       question: 'Do you pee in the shower or bath?',
-      yesVotes: 0,
-      noVotes: 0,
+      yesVotes: 21,
+      noVotes: 57,
     },
     {
       date: '2026-07-31',
